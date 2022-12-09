02f04c8cd365f439c83a9d76436f892907faa0fd
devsumagoinfotech <dev@sumagoinfotech.com>
2022-12-10 03:13:16 +0530

    change gsurvey api

--- routes/generalSurvey.js
+++ routes/generalSurvey.js
@@ -5,7 +5,9 @@ const GeneralSurveyController = require("../controllers/GeneralSurveyController"
 var routerGSurvey = express.Router();
 routerGSurvey.post("/addGeneralSurvey", GeneralSurveyController.addGeneralSurvey);
 routerGSurvey.post("/GeneralSurveyList", GeneralSurveyController.GeneralSurveyList);
-routerGSurvey.post("/screeningScreener", GeneralSurveyController.tmp_out0List);
+routerGSurvey.post("/searchdata", GeneralSurveyController.searchdata);
+routerGSurvey.post("/screeningScreener", GeneralSurveyController.tmp_out0List1);
+routerGSurvey.post("/lungtestCase", GeneralSurveyController.tmpTestData);
 routerGSurvey.post("/screenersevika", GeneralSurveyController.tmp_out1List);
 routerGSurvey.post("/criticallipid", GeneralSurveyController.tmp_outList);
 module.exports = routerGSurvey;
