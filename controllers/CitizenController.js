b99f558b2214f7c761b325c19592693015f33106
Satish <satishaurange@sumagoinfotech.com>
2023-11-27 12:32:20 +0530

    changes for the prescribe list  new updated new

--- controllers/CitizenController.js
+++ controllers/CitizenController.js
@@ -1064,7 +1064,7 @@ exports.CitizenPrescribe = [
 							'info.photo': 1,
 							'screener.firstName': 1,
 							'screener.lastName': 1,
-							'info.updatedAt':1
+							'updatedAt':1
 
 
 						}
