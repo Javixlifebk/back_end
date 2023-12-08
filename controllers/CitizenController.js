7c3a1bab9af538e0496085f613dc38fa8f1205a5
Satish <satishaurange@sumagoinfotech.com>
2023-12-08 17:38:40 +0530

    changes for the website

--- controllers/CitizenController.js
+++ controllers/CitizenController.js
@@ -597,7 +597,7 @@ exports.citizenRefersAllReferAndNonPrescribed = [
 
 
 				CitizenModel.Citizen.aggregate([
-					{ '$match':{'isUnrefer': {$in: ["1","2"]},ngoId:req.body.ngoId}} ,//isunrefer 1 showing data refer list
+					{ '$match':{'isUnrefer': {$in: [1,2]},ngoId:req.body.ngoId}} ,//isunrefer 1 showing data refer list
 
 					{ '$sort': { 'createdAt': -1 } },
 					{ '$limit': 100 },
